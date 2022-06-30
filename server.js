e6c8062f33a19c79ad5acdfbc6ebe48fa41649f2
Katie Brown <katieleebrown19@gmail.com>
2022-06-30 12:11:42 -0400

    added variables and mongo connection

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoClient = require('mongodb').mongoClient
+const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    // Need to update dbName for individual projects
+    dbName = 'crit-role',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        // Need to update text inside collection for individual projects
+        collection = db.collection('characters-and-players')
+    })
